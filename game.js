1a64044561d8f674e7dd3ec6d946bc6df58056dd
k-46 <kamalesh.a@kalvium.community>
2023-08-25 16:42:06 +0530

    made functional

--- game.js
+++ game.js
@@ -1,11 +1,77 @@
-// Iteration 2: Generating two random numbers (0 to 100) and displaying the same in the game.html
+const number1 = document.getElementById("number1");
+const number2 = document.getElementById("number2");
+const number3 =document.getElementById("number3");
+const buttons = document.getElementById("buttons");
+const timer = document.getElementById("timer");
+let time = 20;
+let score = 0;
 
-// Iteration 3: Creating variables required to make the game functional
+let operator;
+function randomNumbers(){
+    return Math.floor(Math.random()*100)+1
+}
 
-// Iteration 4: Creating a variable for number 3 and a variable for storing the html element with the Id "number3"
+function displayNumbers(){
+    let num1 = randomNumbers();
+    let num2 = randomNumbers();
+    let num3 = updateAnswer(num1,num2)
+    number1.innerText=num1;
+    number2.innerText=num2;
+    number3.innerText=num3;
+}
 
-// Iteration 5: Creating a randomise function to make the game functional
+displayNumbers()
 
-// Iteration 6: Making the Operators (button) functional
 
-// Iteration 7: Making Timer functional
+buttons.addEventListener("click",(anything)=>{
+    checkScore(anything.target.id)
+})
+
+
+function checkScore(any){
+    if(any=="plus" && operator=="+" || any=="minus" && operator=="-" || any=="mul" && operator=="*" || any=="divide" && operator=="/" || any=="modulus" && operator=="%"){
+        displayNumbers();
+        time=20;
+        score++;
+        // console.log(localStorage.getItem("score"))
+    }
+    else{
+        window.location.href="gameover.html";
+    }
+    localStorage.setItem("score",score);
+}
+
+timer.innerText=time;
+setInterval(setTimer,1000);
+
+function setTimer(){
+    time--
+    if(time==0){
+        window.location.href="gameover.html";
+    }
+    timer.innerText=time;
+}
+
+
+function updateAnswer(num1,num2){
+    let ans=0;
+    let operators = ["+","-","*","/","%"];
+    operator =operators[Math.floor(Math.random()*5)]
+    if(operator=="+"){
+        ans = num1+num2;
+    }
+    else if(operator=="-"){
+        ans = num1-num2;
+    }
+    else if(operator=="*"){
+        ans = num1*num2;
+    }
+    else if(operator=="/"){
+        ans = Math.floor(num1/num2);
+    }
+    else if(operator=="%"){
+        ans = num1%num2;
+    }
+    return ans
+}
+
--- score.js
+++ score.js
@@ -1,1 +1,8 @@
 // Iteration 8: Making scoreboard functional
+const score = document.getElementById("score-board");
+const playAgain = document.getElementById("play-again-button");
+let currentScore =localStorage.getItem("score");
+score.innerText = currentScore;
+playAgain.onclick = ()=>{
+    window.location.href = "game.html";
+}
